37d59f29134c018fcdaaa2182fd5cdc580c3b057
Ayu Demura <ayud@lexmark.com>
2013-05-28 11:51:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '94c641b08caa6dffe0cac7132eb74b3031f231b2414996fc466151aedb251eccaa52a13689bb98e1b86f1d3687732b3490d636d1d051b6edeb4858e6508e9333'
